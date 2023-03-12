217b0ad2688b4447447d303d7183644fb0c087ad
Cameron Armstrong <cameronscottarmstrong@gmail.com>
2023-03-12 23:19:10 +0800

    Use AmigaOS  3.x Intuition

--- main.c
+++ main.c
@@ -2,15 +2,32 @@
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <proto/graphics.h>
-#include <graphics/view.h>
 #include <proto/intuition.h>
 #include <intuition/intuition.h>
+#include <proto/translator.h>
 
+#define WIN_LEFT_EDGE 0
+#define WIN_TOP_EDGE 0
+#define WIN_WIDTH 640
+#define WIN_MIN_WIDTH 640
+#define WIN_HEIGHT 500
+#define WIN_MIN_HEIGHT 500
 
 struct ExecBase *SysBase;
 struct DosLibrary *DOSBase;
 struct IntuitionBase *IntuitionBase;
 struct GfxBase *GfxBase;
+struct Library *TranslatorBase;
+struct Window *window;
+struct Screen *screen;
+LONG exitCode;
+
+void openLibraries();
+void closeLibraries();
+void configureApp();
+void initVideo();
+void cleanExit(LONG);
+BOOL handleIDCMP(struct Window*);
 
 enum ScreenMode {
 	PAL_NON_INTERLACED = 256,
@@ -24,110 +41,164 @@ struct Config {
 } config;
 
 int main() {
+	exitCode = 0;
 	SysBase = *((struct ExecBase**)4UL);
-	void openLibraries();
-	void closeLibraries();
-	void configureApp();
-	void initVideo(struct Screen**, struct Window**);
-	struct Window *window;
-	struct Screen *screen;
-	struct View *view;
+	ULONG signalMask, winSignal, signals;
+	BOOL done = FALSE;
 
 	openLibraries();
+	if (exitCode)
+		goto exit;
+
 	configureApp();
-	initVideo(&screen, &window);
-	
-	SetRGB4(&(screen->ViewPort), 0, 0x0, 0x1, 0x2);
+
+	initVideo();
+	if (exitCode)
+		goto exit;
+
+	winSignal = 1L << window->UserPort->mp_SigBit;
+	signalMask = winSignal;
 
 	Move(window->RPort, 30, 20);
 
-	char article[] = "This is a test string!";
+	UBYTE article[] = "This is a test string!";
 
 	Text(window->RPort, article, sizeof(article) - 1);
 
-	window->RPort->cp_x = 20;
-	window->RPort->cp_y += 20;
+	do {
+		signals = Wait(signalMask);
 
-	Wait(1 << window->UserPort->mp_SigBit);
+		if (signals & winSignal)
+			done = handleIDCMP(window);
+	} while (!done);
 
-	CloseWindow(window);
+	cleanExit(RETURN_OK);
 
-	FreeMem(screen->Font, sizeof(struct TextAttr));
-	CloseScreen(screen);
-	OpenWorkBench();
-
-	closeLibraries();
-
-	return 0;
+exit:
+	return exitCode;
 }
 
 void openLibraries() {
-	IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 0);
-	if (IntuitionBase == NULL)
-		Exit(RETURN_ERROR);
+	IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 37);
+	if (IntuitionBase == NULL) {
+		cleanExit(RETURN_ERROR);
+		return;
+	}
+		
 
 	GfxBase = (struct GfxBase *)OpenLibrary("graphics.library", 0);
-	if (GfxBase == NULL)
-		Exit(RETURN_ERROR);
+	if (GfxBase == NULL) {
+		cleanExit(RETURN_ERROR);
+		return;
+	}
+
+	TranslatorBase = (struct Library *)OpenLibrary("translator.library", 0);
+	if (TranslatorBase == NULL)
+		cleanExit(RETURN_ERROR);
 }
 
-void initVideo(struct Screen **screen, struct Window **window) {
-	struct TextAttr *font;
-	font = AllocMem(sizeof(struct TextAttr), MEMF_CHIP);
-	font->ta_Name = "topaz.font";
-	font->ta_YSize = TOPAZ_SIXTY;
-	font->ta_Style = FS_NORMAL;
-	font->ta_Flags = FPF_ROMFONT;
+void initVideo() {
+	UWORD pens[] = {~0};
 
-	struct NewScreen NewScreen;
-	NewScreen.LeftEdge = 0;
-	NewScreen.TopEdge = 0;
-	NewScreen.Width = 640;
-	NewScreen.Height = config.screenMode;
-	NewScreen.Depth = 4;
-	NewScreen.DetailPen = 0;
-	NewScreen.BlockPen = 1;
-	NewScreen.ViewModes = HIRES;
-	NewScreen.Type = CUSTOMSCREEN;
-	NewScreen.Font = NULL;
-	NewScreen.DefaultTitle = "openAI";
-	NewScreen.Gadgets = NULL;
-	NewScreen.CustomBitMap = NULL;
+	screen = OpenScreenTags(NULL,
+	SA_Pens, (ULONG)pens,
+	SA_DisplayID, HIRES_KEY,
+	SA_Depth, 2,
+	SA_Title, (ULONG)"OpenAI Amiga",
+	TAG_DONE);
 
-	if ((*screen = (struct Screen *)OpenScreen(&NewScreen)) == NULL)
-		Exit(RETURN_ERROR);
+	if (screen == NULL) {
+		cleanExit(RETURN_ERROR);
+		return;
+	}
 
-	CloseWorkBench();
+	SetRGB4(&(screen->ViewPort), 0, 0x0, 0x1, 0x2);
 
-	struct NewWindow NewWindow;
-	NewWindow.LeftEdge = 0;
-	NewWindow.TopEdge = 0;
-	NewWindow.Width = 640;
-	NewWindow.Height = config.screenMode;
-	NewWindow.DetailPen = 0;
-	NewWindow.BlockPen = 1;
-	NewWindow.Title = NULL;
-	NewWindow.Flags = SMART_REFRESH | ACTIVATE | BORDERLESS | NOCAREREFRESH | BACKDROP;
-	NewWindow.IDCMPFlags = NULL;
-	NewWindow.Type = CUSTOMSCREEN;
-	NewWindow.FirstGadget = NULL;
-	NewWindow.CheckMark = NULL;
-	NewWindow.Screen = *screen;
-	NewWindow.BitMap = NULL;
-	NewWindow.MinWidth = 30;
-	NewWindow.MinHeight = 20;
-	NewWindow.MaxWidth = 600;
-	NewWindow.MaxHeight = 600;
+	window = OpenWindowTags(NULL,
+	WA_Left, WIN_LEFT_EDGE,
+	WA_Top, WIN_TOP_EDGE,
+	WA_Width, WIN_WIDTH,
+	WA_Height, WIN_HEIGHT,
+	WA_MinWidth, WIN_MIN_WIDTH,
+	WA_MinHeight, WIN_MIN_HEIGHT,
+	WA_MaxWidth, ~0,
+	WA_MaxHeight, ~0,
+	WA_CloseGadget, TRUE,
+	WA_SizeGadget, FALSE,
+	WA_DepthGadget, FALSE,
+	WA_DragBar, FALSE,
+	WA_Activate, TRUE,
+	WA_NoCareRefresh, TRUE,
+	WA_IDCMP, IDCMP_CLOSEWINDOW,
+	WA_CustomScreen, screen,
+	WA_Title, "Main Window",
+	WA_ScreenTitle, "Our screen - main window active",
+	TAG_DONE);
 
-	if ((*window = (struct Window *)OpenWindow(&NewWindow)) == NULL)
-		Exit(RETURN_ERROR);
+	if (window == NULL)
+		cleanExit(RETURN_ERROR);
+
+	// struct NewWindow NewWindow;
+	// NewWindow.LeftEdge = 0;
+	// NewWindow.TopEdge = 0;
+	// NewWindow.Width = 640;
+	// NewWindow.Height = config.screenMode - 100;
+	// NewWindow.DetailPen = 0;
+	// NewWindow.BlockPen = 1;
+	// NewWindow.Title = NULL;
+	// NewWindow.Flags = SMART_REFRESH | ACTIVATE | BORDERLESS | NOCAREREFRESH | BACKDROP;
+	// NewWindow.IDCMPFlags = NULL;
+	// NewWindow.Type = CUSTOMSCREEN;
+	// NewWindow.FirstGadget = NULL;
+	// NewWindow.CheckMark = NULL;
+	// NewWindow.Screen = screen;
+	// NewWindow.BitMap = NULL;
+	// NewWindow.MinWidth = 30;
+	// NewWindow.MinHeight = 20;
+	// NewWindow.MaxWidth = 600;
+	// NewWindow.MaxHeight = 600;
+
+	// if ((window = (struct Window *)OpenWindow(&NewWindow)) == NULL)
+	// 	Exit(RETURN_ERROR);
 }
 
 void closeLibraries() {
 	CloseLibrary(IntuitionBase);
 	CloseLibrary(GfxBase);
+	CloseLibrary(TranslatorBase);
 }
 
 void configureApp() {
 	config.screenMode = PAL_NON_INTERLACED;
 }
+
+BOOL handleIDCMP(struct Window *window) {
+	BOOL done = FALSE;
+	struct IntuiMessage *message = NULL;
+	ULONG class;
+
+	while (message = (struct IntuiMessage *)GetMsg(window->UserPort)) {
+		class = message->Class;
+		ReplyMsg((struct Message *)message);
+
+		switch (class) {
+			case IDCMP_CLOSEWINDOW:
+				done = TRUE;
+				break;
+			default:
+				break;
+		}
+	}
+
+	return done;
+}
+
+void cleanExit(LONG returnValue) {
+	// There seems to be a bug with the Exit() call causing the program to guru. Use dirty goto's for now
+	CloseWindow(window);
+	CloseScreen(screen);
+	closeLibraries();
+	exitCode = returnValue;
+	// Exit(returnValue);
+}
+
