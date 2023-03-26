d069c4bc93497612bda4289775016e6f9f6181fd
Cameron Armstrong <cameronscottarmstrong@gmail.com>
2023-03-26 17:38:23 +0800

    Cleanup and remove delays

--- src/openai.c
+++ src/openai.c
@@ -84,21 +84,18 @@ LONG initOpenAIConnector() {
 
     sprintf(printText,"opened utility.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
 	if ((SocketBase = OpenLibrary("bsdsocket.library", 0)) == NULL)
 		return RETURN_ERROR;
 
     sprintf(printText, "opened bsdsocket.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
 	if ((AmiSSLMasterBase = OpenLibrary("AMISSL:libs/amisslmaster.library", AMISSLMASTER_MIN_VERSION)) == NULL)
 		return RETURN_ERROR;
 
     sprintf(printText, "opened amisslmaster.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
     if (!GETINTERFACE(IAmiSSLMaster, AmiSSLMasterBase)) {
         return RETURN_ERROR;
@@ -106,7 +103,6 @@ LONG initOpenAIConnector() {
 
     sprintf(printText, "got amisslmaster.library interface\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
     
     if (InitAmiSSLMaster(AMISSL_CURRENT_VERSION, TRUE) == FALSE) {
         return RETURN_ERROR;
@@ -114,14 +110,12 @@ LONG initOpenAIConnector() {
 		
     sprintf(printText, "initialized amisslmaster.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
 	if ((AmiSSLBase = OpenAmiSSL()) == NULL)
 		return RETURN_ERROR;
 
     sprintf(printText, "opened amissl.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
     if(InitAmiSSL(AmiSSL_ErrNoPtr, &errno, AmiSSL_SocketBase, SocketBase, TAG_DONE) != 0) {
         cleanup();
@@ -130,7 +124,6 @@ LONG initOpenAIConnector() {
 
     sprintf(printText, "initialized amissl.library\n");
 	Write(Output(), (APTR)printText, strlen(printText));
-	Delay(50);
 
 	amiSSLInitialized = TRUE;
     return RETURN_OK;
@@ -192,7 +185,6 @@ LONG connectToOpenAI() {
 					{
                         sprintf(printText, "SSL connection to %s using %s\n", host, SSL_get_cipher(ssl));
                         Write(Output(), (APTR)printText, strlen(printText));
-                        Delay(50);
 
 						/* Certificate checking. This example is *very* basic */
 						if((server_cert = SSL_get_peer_certificate(ssl)))
@@ -201,19 +193,16 @@ LONG connectToOpenAI() {
 
                             sprintf(printText, "Server certificate:\n");
                             Write(Output(), (APTR)printText, strlen(printText));
-                            Delay(50);
 
 							if((str = X509_NAME_oneline(X509_get_subject_name(server_cert), 0, 0)))
 							{
                                 sprintf(printText, "\tSubject: %s\n", str);
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
 								OPENSSL_free(str);
 							}
 							else {
                                 sprintf(printText, "Warning: couldn't read subject name in certificate!\n");
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
                             }
 
 							if((str = X509_NAME_oneline(X509_get_issuer_name(server_cert),
@@ -221,13 +210,11 @@ LONG connectToOpenAI() {
 							{
                                 sprintf(printText, "\tIssuer: %s\n", str);
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
 								OPENSSL_free(str);
 							}
 							else {
                                 sprintf(printText, "Warning: couldn't read issuer name in certificate!\n");
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
                             }
 
 							X509_free(server_cert);
@@ -259,12 +246,10 @@ LONG connectToOpenAI() {
 								/* Dump everything to output */
 								while ((ssl_err = SSL_read(ssl, buffer, sizeof(buffer) - 1)) > 0) {
                                     Write(Output(), (APTR)buffer, strlen(buffer));
-                                    Delay(50);
                                 }
 
                                 sprintf(printText, "\nDone!\n");
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
 
 								/* This is not entirely true, check
 								 * the SSL_read documentation
@@ -274,13 +259,11 @@ LONG connectToOpenAI() {
 							else {
                                 sprintf(printText, "Couldn't write request!\n");
                                 Write(Output(), (APTR)printText, strlen(printText));
-                                Delay(50);
                             }
 						}
 						else {
                             sprintf(printText, "Couldn't get server certificate!\n");
                             Write(Output(), (APTR)printText, strlen(printText));
-                            Delay(50);
                         }
 							
 						/* Send SSL close notification */
@@ -289,7 +272,6 @@ LONG connectToOpenAI() {
 					else {
                         sprintf(printText, "Couldn't establish SSL connection!\n");
                         Write(Output(), (APTR)printText, strlen(printText));
-                        Delay(50);
                     }
 					
 					/* If there were errors, print them */
@@ -302,41 +284,34 @@ LONG connectToOpenAI() {
 				else {
                     sprintf(printText, "Couldn't connect to host!\n");
                     Write(Output(), (APTR)printText, strlen(printText));
-                    Delay(50);
                 }
 
                 sprintf(printText, "before SSL_free()\n");
                 Write(Output(), (APTR)printText, strlen(printText));
-                Delay(50);
 				SSL_free(ssl);
 			}
 			else {
                 sprintf(printText, "Couldn't create new SSL handle!\n");
                 Write(Output(), (APTR)printText, strlen(printText));
-                Delay(50);
             }
 				
             sprintf(printText, "before SSL_CTX_free()\n");
             Write(Output(), (APTR)printText, strlen(printText));
-            Delay(50);
 			SSL_CTX_free(ctx);
 		}
 		else {
             sprintf(printText, "Couldn't create new context!\n");
             Write(Output(), (APTR)printText, strlen(printText));
-            Delay(50);
         }
 
 		BIO_free(bio_err);
 
         sprintf(printText,  "before cleanup()\n");
         Write(Output(), (APTR)printText, strlen(printText));
-        Delay(50);
 		cleanup();
 
     sprintf(printText, "before end of connectToOpenAI()\n");
     Write(Output(), (APTR)printText, strlen(printText));
-    Delay(50);
 	return(ok ? RETURN_OK : RETURN_ERROR);
 }
 
@@ -504,141 +479,3 @@ static void cleanup(void)
 	CloseLibrary((struct Library *)UtilityBase);
 	UtilityBase = NULL;
 }
-
-
-//     // // Create an SSL context
-//     SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-//     if (!ctx) {
-//         // printf("Failed to create SSL context\n");
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text7[] = "created SSL context\n";
-// 	Write(Output(), (APTR)text7, strlen(text7));
-// 	Delay(50);
-
-    // // // Set up the connection
-    // int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    // if (sockfd < 0) {
-    //     // printf("Failed to create a socket\n");
-    //     // SSL_CTX_free(ctx);
-    //     // CleanupAmiSSLA();
-    //     return 1;
-    // }
-
-//     UBYTE text8[] = "created socket\n";
-// 	Write(Output(), (APTR)text8, strlen(text8));
-// 	Delay(50);
-
-//     struct sockaddr_in server_addr;
-//     server_addr.sin_family = AF_INET;
-//     server_addr.sin_port = htons(443);
-//     server_addr.sin_addr.s_addr = inet_addr("api.openai.com"); // Use the IP address of api.openai.com
-
-//     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-//         // printf("Failed to connect to the server\n");
-//         CloseSocket(sockfd);
-//         SSL_CTX_free(ctx);
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text9[] = "connected to server\n";
-// 	Write(Output(), (APTR)text9, strlen(text9));
-// 	Delay(50);
-
-//     // Create an SSL connection
-//     SSL *ssl = SSL_new(ctx);
-//     if (!ssl) {
-//         // printf("Failed to create SSL connection\n");
-//         CloseSocket(sockfd);
-//         SSL_CTX_free(ctx);
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text10[] = "created SSL connection\n";
-// 	Write(Output(), (APTR)text10, strlen(text10));
-// 	Delay(50);
-
-//     // Associate the SSL connection with the socket
-//     SSL_set_fd(ssl, sockfd);
-
-//     // Establish the SSL connection
-//     if (SSL_connect(ssl) <= 0) {
-//         // printf("Failed to establish SSL connection\n");
-//         SSL_free(ssl);
-//         CloseSocket(sockfd);
-//         SSL_CTX_free(ctx);
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text11[] = "established SSL connection\n";
-// 	Write(Output(), (APTR)text11, strlen(text11));
-// 	Delay(50);
-
-    // // Compose the POST request
-    // char request[1024];
-    // sprintf(request,
-    //          "POST /v1/engines/davinci-codex/completions HTTP/1.1\r\n"
-    //          "Host: api.openai.com\r\n"
-    //          "Content-Type: application/json\r\n"
-    //          "Authorization: Bearer %s\r\n"
-    //          "Content-Length: %zu\r\n"
-    //          "\r\n"
-    //          "{\"prompt\": \"Hello, how can I help you today?\"}",
-    //          OPENAI_API_KEY, strlen("{\"prompt\": \"Hello, how can I help you today?\"}"));
-
-//     // Send the POST request
-//     if (SSL_write(ssl, request, strlen(request)) < 0) {
-//         // printf("Failed to send POST request\n");
-//         SSL_shutdown(ssl);
-//         SSL_free(ssl);
-//         CloseSocket(sockfd);
-//         SSL_CTX_free(ctx);
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text12[] = "sent POST request\n";
-// 	Write(Output(), (APTR)text12, strlen(text12));
-// 	Delay(50);
-
-//     // Read the response
-//     char response[4096];
-//     int bytes_read = SSL_read(ssl, response, sizeof(response)-1);
-//     if (bytes_read < 0) {
-//         // printf("Failed to read the response\n");
-//         SSL_shutdown(ssl);
-//         SSL_free(ssl);
-//         CloseSocket(sockfd);
-//         SSL_CTX_free(ctx);
-//         // CleanupAmiSSL();
-//         return 1;
-//     }
-
-//     UBYTE text13[] = "read response\n";
-// 	Write(Output(), (APTR)text13, strlen(text13));
-// 	Delay(50);
-
-//     // Null-terminate the response
-//     response[bytes_read] = '\0';
-
-//     // Print the response
-//     // printf("Response:\n%s\n", response);
-
-// 	Write(Output(), (APTR)response, strlen(response));
-// 	Delay(50);
-//     speakText(response);
-
-//     // Clean up
-//     SSL_shutdown(ssl);
-//     SSL_free(ssl);
-//     CloseSocket(sockfd);
-//     SSL_CTX_free(ctx);
-//     // CleanupAmiSSL();
-// //    return(AmiSSLInitialized);
-//     return 0;
-
