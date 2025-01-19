92b9fdfe8eb043002987fa9a44e9f13d4cebfb5b
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2025-01-19 17:56:57 +0800

    Revert "Make sure docker uses the same user as the host to aboid permission issues"
    This reverts commit 1127eeb0cd6bccdfb38edc03d04976ade48cc745.

--- build_morphos.sh
+++ build_morphos.sh
@@ -9,13 +9,7 @@ if [[ "${CLEAN}" == "1" ]]; then
 	rm -rf build
 fi
 
-# Get the current user ID and group ID. We want ti to be the same as the host user so that
-# the files created by the container have the same permissions as the host user.
-USER_ID=$(id -u)
-GROUP_ID=$(id -g)
-
 docker run --rm \
 	-v ${PWD}:/work \
-	-u ${USER_ID}:${GROUP_ID} \
-	-e USER=${USER_ID} -e GROUP=${GROUP_ID} -e DEBUG=${DEBUG} \
+	-e USER=$(id -u) -e GROUP=$(id -g) -e DEBUG=${DEBUG} \
 	sacredbanana/amiga-compiler:ppc-morphos make -f Makefile.MorphOS
