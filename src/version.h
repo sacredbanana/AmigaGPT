1b73eaa0146ed286122a1da5dce7b749fcda3bcc
Cameron Armstrong (Nightfox) <cameronscottarmstrong@gmail.com>
2025-03-10 17:11:39 +0800

    Complete working installer

--- src/version.h
+++ src/version.h
@@ -6,4 +6,4 @@
     APP_VERSION_MAJOR APP_VERSION_MINOR "." APP_VERSION_PATCH
 #define APP_VERSION                                                            \
     APP_VERSION_MAJOR "." APP_VERSION_MINOR "." APP_VERSION_PATCH
-#define BUILD_NUMBER "10700"
+#define BUILD_NUMBER "10934"
